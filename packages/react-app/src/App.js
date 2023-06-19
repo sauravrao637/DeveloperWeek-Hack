8f2e5eab07a4efb9d9529818e2c3b81919a32e30
camo0112 <56369484+camo0112@users.noreply.github.com>
2023-06-19 08:25:12 +0530

    updated images

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -39,31 +39,31 @@ const Contract1Page = ({ mintNFT, myTotalNFTs }) => {
       <div>myTotalNFTs = {myTotalNFTs}</div>
       <div className="nft-boxes">
         <div className="nft-box">
-          <img src="https://i.ibb.co/Qv9LH8k/img1.jpg" alt="Rare NFT" />
+          <img src="./common.jpg" alt="Rare NFT" />
           <div className="discord-address">DMB#438</div>
           <div className="unclickable-tab">Rare</div> <br />
           <button className="mintNFT" onClick={() => mintNFT(0)}> Mint </button>
         </div>
         <div className="nft-box">
-          <img src="https://i.ibb.co/cYHfvTS/img2.jpg" alt="Rare NFT" />
+          <img src="./uncommon.jpg" alt="Rare NFT" />
           <div className="discord-address">DMB#439</div>
           <div className="unclickable-tab">Common</div> <br />
           <button className="mintNFT" onClick={() => mintNFT(1)}> Mint </button>
         </div>
         <div className="nft-box">
-          <img src="https://i.ibb.co/sH7PVSM/img3.jpg" alt="Rare NFT" />
+          <img src="./rare.jpg" alt="Rare NFT" />
           <div className="discord-address">DMB#440</div>
           <div className="unclickable-tab">Epic</div> <br />
           <button className="mintNFT" onClick={() => mintNFT(2)}> Mint </button>
         </div>
         <div className="nft-box">
-          <img src="https://i.ibb.co/5FL15cJ/img4.jpg" alt="Rare NFT" />
+          <img src="./epic.jpg" alt="Rare NFT" />
           <div className="discord-address">DMB#441</div>
           <div className="unclickable-tab">Rare</div> <br />
           <button className="mintNFT" onClick={() => mintNFT(3)}> Mint </button>
         </div>
         <div className="nft-box">
-          <img src="https://i.ibb.co/C9mN8jc/img5.jpg" alt="Rare NFT" />
+          <img src="./legendary.jpg" alt="Rare NFT" />
           <div className="discord-address">DMB#442</div>
           <div className="unclickable-tab">Legendary</div> <br />
           <button className="mintNFT" onClick={() => mintNFT(4)}> Mint </button>
